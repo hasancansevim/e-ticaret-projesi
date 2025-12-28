04457651b30047fb8cde453b5b6f0077d7a79f5d
Hasan Can Sevim <shasancan0@gmail.com>
2025-12-28 20:32:07 +0300

    added about.php

--- about.php
+++ about.php
@@ -1,30 +1,28 @@
-<?php include("header.php"); ?>
+<?php include 'header.php'; ?>
+
 <section class="bg-success py-5">
     <div class="container">
         <div class="row align-items-center py-5">
             <div class="col-md-8 text-white">
-                <h1>About Us</h1>
+                <h1>Hakkımızda</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Zay Shop olarak, en kaliteli ürünleri en uygun fiyatlarla sizlere sunmak için yola çıktık.
+                    Müşteri memnuniyetini esas alan vizyonumuzla, e-ticaret dünyasında güvenilir bir liman olmayı
+                    hedefliyoruz.
                 </p>
             </div>
             <div class="col-md-4">
-                <img src="assets/img/about-hero.svg" alt="About Hero">
+                <img src="assets/img/about-hero.svg" alt="About Hero" class="img-fluid">
             </div>
         </div>
     </div>
 </section>
-
-<!-- Start Section -->
 <section class="container py-5">
     <div class="row text-center pt-5 pb-3">
         <div class="col-lg-6 m-auto">
-            <h1 class="h1">Our Services</h1>
+            <h1 class="h1">Hizmetlerimiz</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                Lorem ipsum dolor sit amet.
+                Sadece ürün satmıyoruz, size harika bir alışveriş deneyimi sunuyoruz.
             </p>
         </div>
     </div>
@@ -33,63 +31,51 @@
         <div class="col-md-6 col-lg-3 pb-5">
             <div class="h-100 py-5 services-icon-wap shadow">
                 <div class="h1 text-success text-center"><i class="fa fa-truck fa-lg"></i></div>
-                <h2 class="h5 mt-4 text-center">Delivery Services</h2>
+                <h2 class="h5 mt-4 text-center">Hızlı Teslimat</h2>
             </div>
         </div>
 
         <div class="col-md-6 col-lg-3 pb-5">
             <div class="h-100 py-5 services-icon-wap shadow">
                 <div class="h1 text-success text-center"><i class="fas fa-exchange-alt"></i></div>
-                <h2 class="h5 mt-4 text-center">Shipping & Return</h2>
+                <h2 class="h5 mt-4 text-center">Kolay İade</h2>
             </div>
         </div>
 
         <div class="col-md-6 col-lg-3 pb-5">
             <div class="h-100 py-5 services-icon-wap shadow">
                 <div class="h1 text-success text-center"><i class="fa fa-percent"></i></div>
-                <h2 class="h5 mt-4 text-center">Promotion</h2>
+                <h2 class="h5 mt-4 text-center">İndirim Fırsatları</h2>
             </div>
         </div>
 
         <div class="col-md-6 col-lg-3 pb-5">
             <div class="h-100 py-5 services-icon-wap shadow">
                 <div class="h1 text-success text-center"><i class="fa fa-user"></i></div>
-                <h2 class="h5 mt-4 text-center">24 Hours Service</h2>
+                <h2 class="h5 mt-4 text-center">7/24 Destek</h2>
             </div>
         </div>
     </div>
 </section>
-<!-- End Section -->
-
-<!-- Start Brands -->
 <section class="bg-light py-5">
     <div class="container my-4">
         <div class="row text-center py-3">
             <div class="col-lg-6 m-auto">
-                <h1 class="h1">Our Brands</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    Lorem ipsum dolor sit amet.
-                </p>
+                <h1 class="h1">İş Ortaklarımız</h1>
+                <p>Dünyaca ünlü markalarla çalışarak kaliteyi ayağınıza getiriyoruz.</p>
             </div>
             <div class="col-lg-9 m-auto tempaltemo-carousel">
                 <div class="row d-flex flex-row">
-                    <!--Controls-->
                     <div class="col-1 align-self-center">
-                        <a class="h1" href="#templatemo-slide-brand" role="button" data-bs-slide="prev">
+                        <a class="h1" href="#multi-item-example" role="button" data-bs-slide="prev">
                             <i class="text-light fas fa-chevron-left"></i>
                         </a>
                     </div>
-                    <!--End Controls-->
-
-                    <!--Carousel Wrapper-->
                     <div class="col">
-                        <div class="carousel slide carousel-multi-item pt-2 pt-md-0" id="templatemo-slide-brand"
+                        <div class="carousel slide carousel-multi-item pt-2 pt-md-0" id="multi-item-example"
                             data-bs-ride="carousel">
-                            <!--Slides-->
                             <div class="carousel-inner product-links-wap" role="listbox">
 
-                                <!--First slide-->
                                 <div class="carousel-item active">
                                     <div class="row">
                                         <div class="col-3 p-md-5">
@@ -110,73 +96,17 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!--End First slide-->
-
-                                <!--Second slide-->
-                                <div class="carousel-item">
-                                    <div class="row">
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_01.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_02.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_03.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_04.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--End Second slide-->
-
-                                <!--Third slide-->
-                                <div class="carousel-item">
-                                    <div class="row">
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_01.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_02.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_03.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                        <div class="col-3 p-md-5">
-                                            <a href="#"><img class="img-fluid brand-img" src="assets/img/brand_04.png"
-                                                    alt="Brand Logo"></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--End Third slide-->
-
                             </div>
-                            <!--End Slides-->
                         </div>
                     </div>
-                    <!--End Carousel Wrapper-->
-
-                    <!--Controls-->
                     <div class="col-1 align-self-center">
-                        <a class="h1" href="#templatemo-slide-brand" role="button" data-bs-slide="next">
+                        <a class="h1" href="#multi-item-example" role="button" data-bs-slide="next">
                             <i class="text-light fas fa-chevron-right"></i>
                         </a>
                     </div>
-                    <!--End Controls-->
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!--End Brands-->
-
-
-<? include("footer.php") ?>
+<?php include 'footer.php'; ?>
