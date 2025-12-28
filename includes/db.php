81b2f1c113ce9e03ab246f8fd3517971dbefdca0
Hasan Can Sevim <shasancan0@gmail.com>
2025-12-28 23:41:28 +0300

    removed images

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,21 @@
 <?php
+// $host = 'localhost';
+// $dbname = 'eticaret_db';
+// $username = "root";
+// $password = "";
+
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     echo "Bağlantı Hatası" . $e->getMessage();
+//     die();
+// }
+
 $host = 'localhost';
-$dbname = 'eticaret_db';
-$username = "root";
-$password = "";
+$dbname = 'db_121620211041';
+$username = "121620211041";
+$password = "8oE1h7bxtj4";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
